14534c880e5d63d07e7e3b37163ab7a346ad0329
Harry Jarman <me@harryjms.uk>
2021-10-21 22:08:51 +0100

    initial

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <main>
+      <h1>Hello, I'm Harry.</h1>
+      <p>Welcome to my little corner of the internet.</p>
+    </main>
+  );
+};
+
+export default Homepage;
