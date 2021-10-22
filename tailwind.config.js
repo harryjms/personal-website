775246fdb97ab6feed6cf44a0d3772262e98ce96
Harry Jarman <me@harryjms.uk>
2021-10-22 01:19:30 +0100

    dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   mode: "jit",
   purge: ["./src/**/*.{ts,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: "media", // or 'media' or 'class'
   theme: {
     extend: {},
     container: {
