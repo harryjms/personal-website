38f423dfcca3ed9c306936ab80b880c2df5f2697
Harry Jarman <me@harryjms.uk>
2021-10-22 01:21:33 +0100

    Title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,24 @@
 import React from "react";
-
+import Head from "next/head";
 const Homepage = () => {
   return (
-    <main>
-      <h1>Hello, I'm Harry.</h1>
-      <p>
-        Welcome to my little corner of the internet. I'll add some stuff here
-        eventually, but for now it's just a landing page.
-      </p>
-      <h2>About me</h2>
-      <p>
-        I am a Software Engineer living in London and working at the BBC on
-        products that help power radio.
-      </p>
-    </main>
+    <>
+      <Head>
+        <title>Harry</title>
+      </Head>
+      <main>
+        <h1>Hello, I'm Harry.</h1>
+        <p>
+          Welcome to my little corner of the internet. I'll add some stuff here
+          eventually, but for now it's just a landing page.
+        </p>
+        <h2>About me</h2>
+        <p>
+          I am a Software Engineer living in London and working at the BBC on
+          products that help power radio.
+        </p>
+      </main>
+    </>
   );
 };
 
